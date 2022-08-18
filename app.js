0ce66c89b429335d670d826b8189f28b3a483433
CReies <ccreyesmolina@gmail.com>
2022-08-17 21:56:55 -0500

    cors installed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const createError = require('http-errors');
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -10,8 +11,9 @@ const app = express();
 
 // Middlewares
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 require('./db/connection')();
 
 // Routes
+app.use('/api/tasks', require('./routes/tasks'));
 
 // Catch 404 and forward to error handler
 app.use(function (_req, _res, next) {
