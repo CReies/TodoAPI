27febfa35f837c594d8bbfc856ac2802659bcd5e
CReies <ccreyesmolina@gmail.com>
2022-08-29 19:41:18 -0500

    removed unnecessary files and dependences

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const createError = require('http-errors');
 const cors = require('cors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 require('dotenv').config();
@@ -14,8 +12,6 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // DB connect
 require('./db/connection')();
@@ -25,7 +21,7 @@ app.use('/api/tasks', require('./routes/tasks'));
 app.use('/api/categories', require('./routes/categories'));
 
 // Catch 404 and forward to error handler
-app.use(function (_req, _res, next) {
+app.use((_req, _res, next) => {
 	next(createError(404));
 });
 
