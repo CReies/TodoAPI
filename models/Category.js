f85651c0b26faa73991eb628a929116406cf8293
CReies <ccreyesmolina@gmail.com>
2022-09-02 19:50:13 -0500

    deleted override toJson methods and manually _id

--- models/Category.js
+++ models/Category.js
@@ -1,37 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const categorySchema = new Schema({
-	title: {
-		type: String,
-		required: true,
-		minlength: 3,
-		maxlength: 255,
-	},
-	description: {
-		type: String,
-		minlength: 3,
-		maxlength: 255,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-	tasks: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Task',
+const categorySchema = new Schema(
+	{
+		_id: { type: String, default: new mongoose.Types.ObjectId().toString() },
+		title: {
+			type: String,
+			required: true,
+			minlength: 3,
+			maxlength: 255,
 		},
-	],
-});
-
-categorySchema.set('toJSON', {
-	transform: (_document, returnedObject) => {
-		returnedObject.id = returnedObject._id;
-		delete returnedObject._id;
-		delete returnedObject.__v;
+		description: {
+			type: String,
+			minlength: 3,
+			maxlength: 255,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		tasks: [
+			{
+				type: String,
+				ref: 'Task',
+			},
+		],
 	},
-});
+	{ _id: false }
+);
 
 const Category = mongoose.model('Category', categorySchema);
 
--- models/Task.js
+++ models/Task.js
@@ -1,41 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const taskSchema = new Schema({
-	title: {
-		type: String,
-		required: true,
-		minlength: 3,
-		maxlength: 255,
+const taskSchema = new Schema(
+	{
+		_id: { type: String, default: new mongoose.Types.ObjectId().toString() },
+		title: {
+			type: String,
+			required: true,
+			minlength: 3,
+			maxlength: 255,
+		},
+		description: {
+			type: String,
+			minlength: 3,
+			maxlength: 255,
+		},
+		category: {
+			type: String,
+			required: false,
+			minlength: 3,
+			maxlength: 255,
+		},
+		completed: {
+			type: Boolean,
+			default: false,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
 	},
-	description: {
-		type: String,
-		minlength: 3,
-		maxlength: 255,
-	},
-	category: {
-		type: Schema.Types.ObjectId,
-		required: false,
-		minlength: 3,
-		maxlength: 255,
-	},
-	completed: {
-		type: Boolean,
-		default: false,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-});
-
-taskSchema.set('toJSON', {
-	transform: (_document, returnedObject) => {
-		returnedObject.id = returnedObject._id;
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
-});
+	{ _id: false }
+);
 
 const Task = mongoose.model('Task', taskSchema);
 
